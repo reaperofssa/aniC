fa7c1541757d7c5f9ada7058b15583a64a01a1a7
Blame Reiker <reaperofssa@gmail.com>
2025-07-12 20:06:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const app = express();
